16f7e2a1193d2db20a0274fb70e9c9592072c6cb
Otto Jongerius <otto.jongerius@gmail.com>
2016-02-22 23:43:39 +1100

    Three js hello world.

--- main.js
+++ main.js
@@ -52,6 +52,31 @@ function cmd(input) {
 // TODO: Always keep focus on stdin, not just on document load
 //(document).ready(function() {
 $(function() {
+			var scene = new THREE.Scene();
+			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+			var renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
+
+			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true});
+			var cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
+
+			camera.position.z = 5;
+
+			var render = function () {
+				requestAnimationFrame( render );
+
+				cube.rotation.x += 0.1;
+				cube.rotation.y += 0.1;
+
+				renderer.render(scene, camera);
+			};
+
+			render();
+
     var commands = [];
     if ( Cookies.get('last_visit') ) {
         $( '#last' ).text(Cookies.get('last_visit').replace(/(?:\r\n|\r|\n)/g, '').split(','));
